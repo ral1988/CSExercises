f1c4e7c3282321243f527afa09c3d519e4fe29da
Ryan Lee <ryanaleebyu@gmail.com>
2020-01-29 22:45:11 -0500

    finished ex 3a

--- EX3ACSCAlculatingAverages/EX3ACSCAlculatingAverages/Program.cs
+++ EX3ACSCAlculatingAverages/EX3ACSCAlculatingAverages/Program.cs
@@ -3,205 +3,193 @@ using System;
 
 namespace EX3ACSCAlculatingAverages
 {
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            //Sum of numbers
-            double sum = 0, input = 1, numbers = 0.0d;
-            double avg = 0.0d;
+        double sum = 0;
+        double input = 1;
+        double numbers = 0.0d;
+        double sumGrade = 0.0d;
+        double avgGrade = 0.0d;
+        int iteration = 1;
 
-            Console.Write("Input 10 numbers (between 0 and 100) \n");
 
-            do
+        Console.WriteLine("Welcome to the Calculating place");
+        Console.WriteLine("Where your number dreams will come true!");
+        HitAnyKey();
+        Console.WriteLine("We are going to find the sum of 10 numbers");
+        Console.Write("Input 10 numbers (between 0 and 100) \n");
+
+        do
+        {
+            Console.Write($"Number {input}: ");
+            numbers = double.Parse(Console.ReadLine());
+            if ((numbers < 0) || (numbers > 100))
             {
-                Console.Write($"Number {input}: ");
-                numbers = double.Parse(Console.ReadLine());
-                if (numbers < 0)
-                {
-                    Console.WriteLine("Please provide a number that is greater than 0 and less than 100" );
-                    continue;
-                }
-                if (numbers > 100)
-                {
-                    Console.WriteLine("Please provide a number that is greater than 0 and less than 100");
-                    continue;
-                }
-                else
+                Console.WriteLine("Please provide a number that is greater than 0 and less than 100");
+                continue;
+            }
+
+            else
 
                 sum += numbers;
-                ++input;
-            } while (input < 11);
+            ++input;
+        } while (input < 11);
 
+        
+        Console.WriteLine($"The sum of 10 numbers is : {sum}");
+        HitAnyKey();
 
-            Console.WriteLine($"The sum of 10 numbers is : {sum}");
+        Console.Write("Input 10 test grades (between 0 and 100) \n");
 
-            //Average Test Score
-            double sumGrade = 0.0d;
-            input = 1;
-            numbers = 0.0d;
-            double avgGrade = 0.0d;
+        sum = 0;
+        input = 1;
+        numbers = 0.0d;
+        sumGrade = 0.0d;
+        avgGrade = 0.0d;
+        iteration = 1;
 
-            Console.Write("Input 10 test grades (between 0 and 100) \n");
-
-            do
+        do
+        {
+            Console.Write($"Test Grade {input}: ");
+            numbers = double.Parse(Console.ReadLine());
+            if ((numbers < 0) || (numbers > 100))
             {
-                Console.Write($"Test Grade {input}: ");
-                numbers = double.Parse(Console.ReadLine());
-                if (numbers < 0)
-                {
-                    Console.WriteLine("Please provide a number that is greater than 0 and less than 100");
-                    continue;
-                }
-                if (numbers > 100)
-                {
-                    Console.WriteLine("Please provide a number that is greater than 0 and less than 100");
-                    continue;
-                }
-                else
-
-                    sumGrade += numbers;
-                ++input;
-            } while (input < 11);
-
-            avgGrade = sumGrade / 10.0;
-
-            Console.WriteLine($"The average numberical grade: {avgGrade}\n");
-
-            if ((avgGrade >= 95) && (avgGrade <= 100))
-            {
-                Console.WriteLine("Average letter grade is A");
-                return;
-            }
-            if ((avgGrade >= 90) && (avgGrade < 95))
-            {
-                Console.WriteLine("Average letter grade is A-");
-                return;
-            }
-            if ((avgGrade >= 85) && (avgGrade < 90))
-            {
-                Console.WriteLine("Average letter grade is B");
-                return;
-            }
-            if ((avgGrade >= 80) &&(avgGrade < 85))
-            {
-                Console.WriteLine("Average letter grade is B-");
-                return;
-            }
-            if ((avgGrade >= 75) && (avgGrade < 80))
-            {
-                Console.WriteLine("Average letter grade is C");
-                return;
-            }
-            if ((avgGrade >= 70) && (avgGrade < 75))
-            {
-                Console.WriteLine("Average letter grade is C-");
-                return;
-            }
-            if ((avgGrade >= 65) && (avgGrade < 70))
-            {
-                Console.WriteLine("Average letter grade is D");
-                return;
-            }
-            if ((avgGrade >= 60) && (avgGrade < 65))
-            {
-                Console.WriteLine("Average letter grade is D-");
-                return;
+                Console.WriteLine("Please provide a number that is greater than 0 and less than 100");
+                continue;
             }
             else
+
+                sumGrade += numbers;
+            ++input;
+        } while (input < 11);
+
+        avgGrade = sumGrade / 10.0;
+
+        Console.WriteLine($"The average numberical grade: {avgGrade}\n");
+
+        GradeAverage(avgGrade);
+        HitAnyKey();
+
+        sum = 0;
+        input = 1;
+        numbers = 0.0d;
+        sumGrade = 0.0d;
+        avgGrade = 0.0d;
+        iteration = 1;
+
+        Console.Write("How many tests are you entering in?: ");
+        var testCount = Console.ReadLine();
+        int testNumber = int.Parse(testCount);
+
+        Console.Write("Input grade values (between 0 and 100) \n");
+
+        do
+        {
+            Console.Write($"Test Grade-{iteration}: ");
+            numbers = Convert.ToDouble(Console.ReadLine());
+            if ((numbers < 0) || (numbers > 100))
             {
-                Console.WriteLine("Students are failing");
-                return;
+                Console.WriteLine("Please provide a number that is greater than 0 and less than 100");
+                continue;
             }
+            else
 
-            //Average a specific number of scores
-            //The teacher will input how many tests, and then the scores. 
-            //Output will show the average numerical and letter grade. 
-            //double sumGrade = 0.0d;
-            //iteration = 1;
-            //numbers = 0.0d;
-            //double avgGrade = 0.0d;
-            //Console.Writline("How many tests are you entering in?: ");
-            //
-            //Console.Write("Input 10 numbers (between 0 and 100) \n");
+                sumGrade += numbers;
+            ++iteration;
+        } while (iteration < (testNumber + 1));
 
-            //do
-            //{
-            //start:
-            //    Console.Write($"Test Grade-{iteration}: ");
-            //    numbers = Convert.ToInt32(Console.ReadLine());
-            //    if (numbers < 0)
-            //    {
-            //        Console.WriteLine("Please provide a number that is greater than 0 and less than 100");
-            //        goto start;
-            //    }
-            //    if (numbers > 100)
-            //    {
-            //        Console.WriteLine("Please provide a number that is greater than 0 and less than 100");
-            //        goto start;
-            //    }
-            //    else
+        avgGrade = sumGrade / testNumber;
 
-            //        sumGrade += numbers;
-            //    ++input;
-            //} while (iteration < 11);
+        Console.WriteLine($"The average numberical grade: {avgGrade}\n");
+        GradeAverage(avgGrade);
+        HitAnyKey();
 
-            //avgGrade = sumGrade / 10.0;
+        Console.Write("Input grade values (between 0 and 100) \n");
+        Console.WriteLine("To quit, type 'quit'");
 
-            //Console.WriteLine($"The average numberical grade: {avgGrade}\n");
+        sum = 0;
+        numbers = 0.0d;
+        sumGrade = 0.0d;
+        avgGrade = 0.0d;
+        iteration = 1;
+        do
+        {
+            Console.Write($"Test Grade-{iteration}: ");
+            string nums = Console.ReadLine();
+            if (nums == "quit")
+            {
+                break;
+            }
+            numbers = double.Parse(nums);
+            if ((numbers < 0) || (numbers > 100))
+            {
+                Console.WriteLine("Please provide a number that is greater than 0 and less than 100");
+                continue;
+            }
+            else
 
-            //if (avgGrade > 95)
-            //{
-            //    Console.WriteLine("Average letter grade is A");
-            //    return;
-            //}
-            //if (avgGrade > 90)
-            //{
-            //    Console.WriteLine("Average letter grade is A-");
-            //    return;
-            //}
-            //if (avgGrade > 85)
-            //{
-            //    Console.WriteLine("Average letter grade is B");
-            //    return;
-            //}
-            //if (avgGrade > 80)
-            //{
-            //    Console.WriteLine("Average letter grade is B-");
-            //    return;
-            //}
-            //if (avgGrade > 75)
-            //{
-            //    Console.WriteLine("Average letter grade is C");
-            //    return;
-            //}
-            //if (avgGrade > 70)
-            //{
-            //    Console.WriteLine("Average letter grade is C-");
-            //    return;
-            //}
-            //if (avgGrade > 65)
-            //{
-            //    Console.WriteLine("Average letter grade is D");
-            //    return;
-            //}
-            //if (avgGrade > 60)
-            //{
-            //    Console.WriteLine("Average letter grade is D-");
-            //    return;
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Students are failing");
-            //    return;
-            //}
+            sumGrade += numbers;
+            ++iteration;
+        } while (iteration > 1);
 
+        avgGrade = sumGrade / (iteration - 1);
 
+        Console.WriteLine($"The average numberical grade: {avgGrade}\n");
 
+        GradeAverage(avgGrade);
+        HitAnyKey();
+    }
 
+    private static void HitAnyKey()
+    {
+        Console.WriteLine("Hit any key to continue");
+        Console.ReadKey();
+        Console.Clear();
+    }
 
+    private static void GradeAverage(double avgGrade)
+    {
+        if ((avgGrade >= 95) && (avgGrade <= 100))
+        {
+            Console.WriteLine("Average letter grade is A");
+        }
+        if ((avgGrade >= 90) && (avgGrade < 95))
+        {
+            Console.WriteLine("Average letter grade is A-");
+        }
+        if ((avgGrade >= 85) && (avgGrade < 90))
+        {
+            Console.WriteLine("Average letter grade is B");
+        }
+        if ((avgGrade >= 80) && (avgGrade < 85))
+        {
+            Console.WriteLine("Average letter grade is B-");
+        }
+        if ((avgGrade >= 75) && (avgGrade < 80))
+        {
+            Console.WriteLine("Average letter grade is C");
+        }
+        if ((avgGrade >= 70) && (avgGrade < 75))
+        {
+            Console.WriteLine("Average letter grade is C-");
+        }
+        if ((avgGrade >= 65) && (avgGrade < 70))
+        {
+            Console.WriteLine("Average letter grade is D");
+        }
+        if ((avgGrade >= 60) && (avgGrade < 65))
+        {
+            Console.WriteLine("Average letter grade is D-");
+        }
+        if (avgGrade < 60)
+        {
+            Console.WriteLine("Students are failing");
         }
     }
+    
 }
 
 
