fa53720b1d1665f4eccb7b3b456f332401b428e6
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-03-10 09:41:08 -0400

    Added checking method for Int values and checking the range

---
+++ EX9ACS GuessMyNumbe Game(Bisection Algorithm)/BisectionAlgorithm/BisectionAlgorithm/CheckNumbers.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace BisectionAlgorithm
+{
+    class CheckNumbers
+    {
+        CheckNumbers()
+        {
+
+        }
+
+        public int IntCheck()
+        {
+            //check for valid input and prompt end user with correction
+            int value = 0;
+            bool returnError = false;
+
+            do
+            {
+                string userInput = Console.ReadLine();
+                bool quickCheck = int.TryParse(userInput, out _);
+
+                if (quickCheck == true)
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input. Please try again and provide a integer number: ");
+                }
+            } while (returnError == false);
+
+            return value;
+
+        }
+
+        public int InbetweenNumbersCheck(int a, int b)
+        {
+            int value = 0;
+            bool returnError = false;
+
+            do
+            {
+                string userInput = Console.ReadLine();
+                bool quickCheck = int.TryParse(userInput, out _);
+
+                if (quickCheck == true)
+                {
+                    value = int.Parse(userInput);
+
+                    if (value >= a && value <= b)
+                    {
+                        returnError = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Numbers provided are out of range. Please provide different numbers: ");
+                    }
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input. Please try again and provide a integer number: ");
+                }
+            } while (returnError == false);
+
+            return value;
+
+        
+        }
+    }
+}
--- EX9ACS GuessMyNumbe Game(Bisection Algorithm)/BisectionAlgorithm/BisectionAlgorithm/Program.cs
+++ EX9ACS GuessMyNumbe Game(Bisection Algorithm)/BisectionAlgorithm/BisectionAlgorithm/Program.cs
@@ -4,52 +4,9 @@ namespace BisectionAlgorithm
 {
     class Program
     {
-        public double c;
-        public double Func(double x)
-        {
-            double function = x * x * x - 2 * x * x + 3;
-            return function;
-        }
-        public void bisection(double a, double b, double e)
-        {
-            Program func = new Program();
-            if (func.Func(a) * func.Func(b) >= 0)
-            {
-                Console.WriteLine("Incorrect a and b");
-                return;
-            }
-            c = a;
-            while ((b - a) >= e)
-            {
-                c = (a + b) / 2;
-                if (func.Func(c) == 0.0)
-                {
-                    Console.WriteLine("Root = " + c);
-                    break;
-                }
-                else if (func.Func(c) * func.Func(a) < 0)
-                {
-                    Console.WriteLine("Root = " + c);
-                    b = c;
-                }
-                else
-                {
-                    Console.WriteLine("Root = " + c);
-                    a = c;
-                }
-            }
-        }
         public static void Main(string[] args)
         {
-            double a, b, e;
-            Console.WriteLine("Enter the desired accuracy:");
-            e = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the lower limit:");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the upper limit:");
-            b = Convert.ToDouble(Console.ReadLine());
-            Program bisec = new Program();
-            bisec.bisection(a, b, e);
+       
         }
     }
 }
