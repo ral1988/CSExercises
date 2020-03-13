c605476c261ca35ee27f06e829cf205778d31c07
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-03-13 09:31:05 -0400

    Removed list in InitialGame file

--- EX9ACS GuessMyNumbe Game(Bisection Algorithm)/BisectionAlgorithm/BisectionAlgorithm/InitialGame.cs
+++ EX9ACS GuessMyNumbe Game(Bisection Algorithm)/BisectionAlgorithm/BisectionAlgorithm/InitialGame.cs
@@ -20,7 +20,6 @@ namespace BisectionAlgorithm
         static InitialGame()
         {
             var check = new CheckNumbers();
-            List<int> tenInts = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             
             Console.WriteLine("I am going to show you how I can guess your number using the bisection algorithm");
             Console.WriteLine("Enter a number between 1-10: ");
