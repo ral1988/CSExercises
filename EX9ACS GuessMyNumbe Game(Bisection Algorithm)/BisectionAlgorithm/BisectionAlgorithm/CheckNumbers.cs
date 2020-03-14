0634875c69c48374f7a2a17880de23e795fc6931
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-03-13 22:53:02 -0400

    got rid of unused method in CheckNumbers

--- EX9ACS GuessMyNumbe Game(Bisection Algorithm)/BisectionAlgorithm/BisectionAlgorithm/CheckNumbers.cs
+++ EX9ACS GuessMyNumbe Game(Bisection Algorithm)/BisectionAlgorithm/BisectionAlgorithm/CheckNumbers.cs
@@ -9,39 +9,13 @@ namespace BisectionAlgorithm
 
         }
 
-        public int IntCheck(string a)
-        {
-            //check for valid input and prompt end user with correction
-            int value = 0;
-            bool returnError = false;
-
-            do
-            {
-                string userInput = a;
-                bool quickCheck = int.TryParse(userInput, out _);
-
-                if (quickCheck == true)
-                {
-                    return value;
-                }
-                else
-                {
-                    Console.WriteLine("Not a valid input. Please try again and provide a integer number: ");
-                }
-            } while (returnError == false);
-
-            return value;
-
-        }
-
-        public int InbetweenNumbersCheck(int a, int b, string input)
+       public int InbetweenNumbersCheck(int a, int b, string input)
         {
             int value = 0;
             bool returnError = false;
 
             string userInput = input;
 
-            IntCheck(userInput);
 
             do
             {
