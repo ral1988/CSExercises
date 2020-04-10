d6060eaf8efb1d5c07785e401b36d896304af004
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-04-10 09:11:11 -0400

    added example code from MS docs for MD5 hasing. Did some minor refactoring of the code

--- EX3ACSPasswordEncryptionAndAuthentication/EX3ACSPasswordEncryptionAndAuthentication/GetMd5Hash.cs
+++ EX3ACSPasswordEncryptionAndAuthentication/EX3ACSPasswordEncryptionAndAuthentication/GetMd5Hash.cs
@@ -1,4 +1,8 @@
-﻿namespace EX3ACSPasswordEncryptionAndAuthentication
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EX3ACSPasswordEncryptionAndAuthentication
 {
     public class GetMd5Hash
     {
@@ -7,9 +11,37 @@
 
         }
 
-        public void TestHash()
+        public static string MD5Hash(MD5 md5Hash, string input)
         {
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
 
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            foreach (var t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+
+
+        }
+
+        static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = MD5Hash(md5Hash, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return 0 == comparer.Compare(hashOfInput, hash);
         }
     }
 }
