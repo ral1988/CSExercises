bcb3d14f0bd94d158df378fbbc2fb634619a3bbf
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-04-25 10:31:44 -0400

    created input for input strings

--- ISTA421EX4A_Encrypting_and_Decrypting_Messages/ISTA421EX4A_Encrypting_and_Decrypting_Messages/Program.cs
+++ ISTA421EX4A_Encrypting_and_Decrypting_Messages/ISTA421EX4A_Encrypting_and_Decrypting_Messages/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Transactions;
 
 namespace ISTA421EX4A_Encrypting_and_Decrypting_Messages
 {
@@ -10,9 +11,9 @@ namespace ISTA421EX4A_Encrypting_and_Decrypting_Messages
             string singleKey = Util.GetSingleKey();
             string multiKey = Util.GetMultiJKey();
 
-            Console.WriteLine($"You entered {plainText} as plain text");
-            Console.WriteLine($"You entered {singleKey} as your single key");
-            Console.WriteLine($"You entered {multiKey} as your multi key");
+            Console.WriteLine($"You entered [{plainText}] as plain text");
+            Console.WriteLine($"You entered [{singleKey}] as your single key");
+            Console.WriteLine($"You entered [{multiKey}]as your multi key");
             Console.WriteLine();
 
             int[] cleanText = Util.Clean(plainText);
@@ -23,18 +24,18 @@ namespace ISTA421EX4A_Encrypting_and_Decrypting_Messages
             string encMulti = Util.MultiEnc(cleanText, cleanMKey);
             string encConti = Util.ContiEnc(cleanText, cleanMKey);
 
-            Console.WriteLine($"Encrypted message with single key is {encSingle}");
-            Console.WriteLine($"Encrypted message with multi key is {encMulti}");
-            Console.WriteLine($"Encrypted message with continuous key is {encConti}");
+            Console.WriteLine($"Encrypted message with single key is [{encSingle}]");
+            Console.WriteLine($"Encrypted message with multi key is [{encMulti}]");
+            Console.WriteLine($"Encrypted message with continuous key is [{encConti}]");
             Console.WriteLine();
 
             string decSingle = Util.SingleDec(encSingle, cleanSKey);
             string decMulti = Util.MultiDec(encMulti, cleanMKey);
             string decConti = Util.ContiDec(encConti, cleanMKey);
 
-            Console.WriteLine($"Decrypted message with single key is {decSingle}");
-            Console.WriteLine($"Decrypted message with multi key is {decMulti}");
-            Console.WriteLine($"Decrypted message with continuous key is {decConti}");
+            Console.WriteLine($"Decrypted message with single key is [{decSingle}]");
+            Console.WriteLine($"Decrypted message with multi key is [{decMulti}]");
+            Console.WriteLine($"Decrypted message with continuous key is [{decConti}]");
             Console.WriteLine();
         }
     }
@@ -43,17 +44,23 @@ namespace ISTA421EX4A_Encrypting_and_Decrypting_Messages
     {
         public static string GetPlainText()
         {
-            throw new NotImplementedException();
+            Console.Write("Enter plain text: ");
+            var plainText = Console.ReadLine();
+            return plainText;
         }
 
         public static string GetSingleKey()
         {
-            throw new NotImplementedException();
+            Console.Write("Enter your single key as an alpha character: ");
+            var singleKey = Console.ReadLine();
+            return singleKey;
         }
 
         public static string GetMultiJKey()
         {
-            throw new NotImplementedException();
+            Console.Write("Enter your multi key as alpha characters: ");
+            var multiKey = Console.ReadLine();
+            return multiKey;
         }
 
         public static string ContiEnc(int[] cleanText, int[] cleanMKey)
@@ -71,7 +78,7 @@ namespace ISTA421EX4A_Encrypting_and_Decrypting_Messages
             throw new NotImplementedException();
         }
 
-        public static int[] Clean(string multiKey)
+        public static int[] Clean(string stringInput)
         {
             throw new NotImplementedException();
         }
